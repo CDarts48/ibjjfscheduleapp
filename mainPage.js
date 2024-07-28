f7afccf1a0a51b78446f525ae80f2cb71c114d92
Corey <CDartswebdev@gmail.com>
2024-07-28 09:41:39 -0600

    revert

--- mainPage.js
+++ mainPage.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const { getAllEastonCompetitors } = require('/Users/corey/Desktop/masterApp/matches.js'); // replace with the path to your script
-const urls = require('./urls');
+const { getAllEastonCompetitors } = require('./matches.js'); // replace with the path to your script
 
 const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
+    const urls = [
+        'https://www.bjjcompsystem.com/tournaments/2456/tournament_days/3330',
+        'https://www.bjjcompsystem.com/tournaments/2456/tournament_days/3330?page=2',
+        'https://www.bjjcompsystem.com/tournaments/2456/tournament_days/3330?page=3'
+    ];
+
     try {
-        const teamInfo = await getAllEastonCompetitors(urls); // Pass urls here
-        console.log(teamInfo); // Add this line
+        const teamInfo = await getAllEastonCompetitors(urls);
         res.render('index', { teamInfo }); // replace 'index' with the name of your EJS file
     } catch (error) {
         console.error(`Error in promise: ${error.message}`);
--- urls.js
+++
@@ -1,7 +0,0 @@
-const urls = [
-    'https://www.bjjcompsystem.com/tournaments/2456/tournament_days/3330',
-    'https://www.bjjcompsystem.com/tournaments/2456/tournament_days/3330?page=2',
-    'https://www.bjjcompsystem.com/tournaments/2456/tournament_days/3330?page=3'
-];
-
-module.exports = urls;
